c497c3069ae6972dbd7259813ffe4a50ac799e87
Jed Lin <jedlin@boyu66.net>
2019-03-05 13:49:48 +0800

    refactor: extract Load() to IRepository

--- DDD.Example.LightShop/SharedKernel/IRepository.cs
+++ DDD.Example.LightShop/SharedKernel/IRepository.cs
@@ -1,7 +1,11 @@
+using System;
+using DDD.Example.LightShop.OrderContext.Domain;
+
 namespace DDD.Example.LightShop.SharedKernel
 {
     public interface IRepository<T> where T: EntityBase
     {
         void Save(T entity);
+        T Load(Guid id);
     }
 }
